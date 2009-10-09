82eb7324ecf1aa501b52abb42ac2ee5581266c9b
Domino <Domino@Delorum3.local>
2009-10-09 15:49:20 -0600

    embeded flash

--- guide/includes/case-studies.php
+++ guide/includes/case-studies.php
@@ -1,5 +1,5 @@
 <?php
-	$mediaUrl = '/delorum_site/guide';
+	$imgUrl = '/delorum_site/guide';
 ?>
 
 <div id='' class='wrapper right-side-wrapper'>
@@ -7,14 +7,18 @@
 		<?php include('contact-us.php'); ?>
 		
 		<div id='' class='case-studies'>
-			<img src='<?php echo $mediaUrl; ?>/images/right-side/trees.png' alt='trees'/>
+			<img src='<?php echo $imgUrl; ?>/images/right-side/trees.png' alt='trees'/>
 			
-			<script type="text/javascript" src="<?php echo $mediaUrl; ?>/js/swfobject/swfobject.js"></script>
-			<script type="text/javascript">
-	            swfobject.embedSWF("http://192.168.1.20/delorum_site/guide/flash/case-studies/case-studies.swf", "flash-div", "350", "200", "9.0.0", "http://192.168.1.20/delorum_site/guide/js/swfobject/expressInstall.swf");
-	        </script>
+			<div id='case-studies-flash-div' class=''>
+				
+			</div>
 			
 			<?php include('recent-news.php'); ?>
 		</div>
 	</div>
 </div>
+
+<script type="text/javascript" src="<?php echo $imgUrl; ?>/js/swfobject/swfobject.js"></script>
+<script type="text/javascript">
+    swfobject.embedSWF("<?php echo $imgUrl; ?>/flash/case-studies/case-studies.swf", "case-studies-flash-div", "215", "325", "9.0.0", "expressInstall.swf");
+</script>
